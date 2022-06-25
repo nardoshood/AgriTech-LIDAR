57a77fe68739df1daddecedb8ca28acdefbbfe55
nardoshood <nardostilahun83@gmail.com>
2022-06-25 19:30:27 +0000

    add error handling and additional info

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,5 +1,6 @@
 import pdal
 import json
+import os
 from helper_methods import read_json  as read_pipeline
 
 class pydal_fetch():
@@ -8,10 +9,28 @@ class pydal_fetch():
     data fetching using 
     pdal library
     """
-    def __init__(self,bounds,filename):
+    def __init__(self,bounds,region,verbose=False):
         self.bounds = bounds
-        self.filename = filename
+        self.region = region
+        self.verbose = verbose
+        self.data_location = os.path.join("https://s3-us-west-2.amazonaws.com/usgs-lidar-public",self.region,"ept.json")
         self.pipeline_path = "../data/pipline.json"
+        self.prepare_pipeline()
+
+    def fetch_data(self):
+        """
+        fetch data
+        """
+        try:
+            self.execute_pipeline()
+            if self.verbose:
+                self.pipeline_info()
+                print("Executed pipeline successfully")
+                print(self.arrays)
+                print(self.metadata)
+        except Exception as e:
+            print("Failed to execute.")
+            print(e)
 
     def prepare_pipeline(self):
         """
@@ -20,10 +39,16 @@ class pydal_fetch():
         fetching
         """
         self.pipeline_dict = read_pipeline(self.pipeline_path)
-        self.pipeline_dict["pipeline"][0]["filename"] = self.filename
+        self.pipeline_dict["pipeline"][0]["filename"] = self.data_location
         self.pipeline_dict["pipeline"][0]["bounds"] = self.bounds
         pipeline_input = self.prepare_pipeline_output()
-        self.pipeline = pdal.Pipeline(pipeline_input)
+        try:
+            self.pipeline = pdal.Pipeline(pipeline_input)
+            if self.verbose:
+                print(pipeline_input)
+        except Exception as e:
+            print("Failed to prepare the pipeline.")
+            print(e)
         # self.pipeline_dict["pipeline"][0]["filename"] = self.filename
 
     def prepare_pipeline_output(self):
